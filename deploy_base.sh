253ac2874a4e9268a3e660815116925725efa004
GeorgeQing <taoqinget@gmail.com>
2025-06-20 19:19:06 +0800

    refactor: update deploy script for base

--- deploy_base.sh
+++ deploy_base.sh
@@ -7,14 +7,13 @@ source .env
 poolManagerAddress=0x498581ff718922c3f8e6a244956af099b2652b2b
 positionManagerAddress=0x7c5f5a4bbd8fd63184577525326123b519429bdc
 permit2Address=0x000000000022D473030F116dDEE9F6B43aC78BA3
-
-protocolOwnerAddress=0x022Ca046a4452cCc4C578eb430A60C660ba1b74d
-platformReserveAddress=0x022Ca046a4452cCc4C578eb430A60C660ba1b74d
-
+universalRouterAddress=0x6fF5693b99212Da76ad316178A184AB56D299b43
+platformReserveAddress=0x169Fb46B8da6571b9fFF3026A774FCB9f96A528c
+protocolOwnerAddress=0x169Fb46B8da6571b9fFF3026A774FCB9f96A528c
 baseTokenURI="ipfs://"
 
 # Set the local solc compiler version
 solcVersion="0.8.26+commit.8a97fa7a"
 
 forge create --chain 8453 --rpc-url $BASE_RPC_URL --via-ir --optimizer-runs 100 --compiler-version $solcVersion --interactive --broadcast --verify src/FairLaunchFactoryV2.sol:FairLaunchFactoryV2 \
-    --constructor-args $poolManagerAddress $platformReserveAddress $positionManagerAddress $permit2Address $protocolOwnerAddress $baseTokenURI
+    --constructor-args $poolManagerAddress $positionManagerAddress $permit2Address $universalRouterAddress $platformReserveAddress $protocolOwnerAddress $baseTokenURI
