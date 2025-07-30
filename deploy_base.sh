c7177785ad7061720e78fa1ec287e9fd3f16de5a
Mikołaj Woźniak <mikolajw@glitchd.network>
2025-07-30 12:21:14 +0200

    Add new remote and local

--- deploy_base.sh
+++ deploy_base.sh
@@ -15,8 +15,8 @@ baseTokenURI="ipfs://"
 # Set the local solc compiler version
 solcVersion="0.8.26+commit.8a97fa7a"
 
-forge create --chain 8453 --rpc-url $BASE_RPC_URL --via-ir --optimizer-runs 100 --compiler-version $solcVersion --interactive --broadcast --verify src/FairLaunchFactoryV2.sol:FairLaunchFactoryV2 \
-    --constructor-args $poolManagerAddress $positionManagerAddress $permit2Address $universalRouterAddress $platformReserveAddress $protocolOwnerAddress $baseTokenURI
+# forge create --chain 8453 --rpc-url $BASE_RPC_URL --via-ir --optimizer-runs 100 --compiler-version $solcVersion --interactive --broadcast --verify src/FairLaunchFactoryV2.sol:FairLaunchFactoryV2 \
+#     --constructor-args $poolManagerAddress $positionManagerAddress $permit2Address $universalRouterAddress $platformReserveAddress $protocolOwnerAddress $baseTokenURI
 
 encodedArgs=$(cast abi-encode \
   "constructor(address,address,address,address,address,address,string)" \
@@ -31,22 +31,22 @@ encodedArgs=$(cast abi-encode \
 forge build --via-ir --optimizer-runs 100 --compiler-version $solcVersion \
 && local=$(forge inspect src/FairLaunchFactoryV2.sol:FairLaunchFactoryV2 bytecode | sed 's/^0x//') \
 && args=$(echo "$encodedArgs" | sed 's/^0x//') \
-&& echo "local creation-hash: $(echo ${local}${args} | cast keccak)" \
+&& echo "local creation-hash: $(echo ${local} | cast keccak)" \
 && echo "0x${local}" > local.hex \
 && remote=$(tr -d '\n' < remote.hex | sed 's/^0x//') \
 && echo "remote creation-hash: $(echo ${remote} | cast keccak)" \
-&& if [ "${local}${args}" = "$remote" ]; then \
+&& if [ "${local}" = "$remote" ]; then \
        echo "Byte‑code matches remote.hex"; \
    else \
        echo "Byte‑code differs from remote.hex"; \
        echo "first 100 differing bytes (index: local | remote)"; \
-       diff <(echo ${local}${args} | fold -w2 | nl -ba) <(echo $remote | fold -w2 | nl -ba) | head -100; \
+       diff <(echo ${local} | fold -w2 | nl -ba) <(echo $remote | fold -w2 | nl -ba) | head -100; \
        exit 1; \
    fi
 
 # WARNING: The Etherscan verification step can occasionally fail (e.g., API rate limits?).
 #          This **only** verifies an already‑deployed contract.
-# deployedContractAddress="0x5Fe424f5982b93676D6d359187A2c5dd251a6c28"
+# deployedContractAddress="0xD8EDBb1eeC4dD4778f4642320951ec88beA625F7"
 # forge verify-contract \
 #     --chain 8453 \
 #     --compiler-version $solcVersion \
